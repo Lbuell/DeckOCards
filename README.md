91d57c31618412e4e2b393efe498c597a439d92c
lbuell <liamcbuell@gmail.com>
2014-01-25 14:55:43 -0800

    Finished

---
+++ README.md
@@ -0,0 +1,11 @@
+# Basic Ruby App to sort a deck of cards by suit
+<!-- If you'd like to use a logo instead uncomment this code and remove the text above this line
+
+  ![Logo](URL to logo img file goes here)
+
+-->
+
+By [Liam Buell](http://www.liambuell.com).
+
+## Description
+**Ruby App** A bare-bones ruby file that will sort a deck of cards by rank and suit
--- lib/deck.rb
+++ lib/deck.rb
@@ -1,7 +1,38 @@
-class Deck
-  attr_accessor :sort
+class Card
 
-  def initialize
-    @sort = "Welcome to ruby"
+  SUITS = %w{ hearts diamonds spades clubs }
+  RANKS = %w{ 2 3 4 5 6 7 8 9 10 J Q K A }
+
+  attr_reader :rank, :suit
+
+  def initialize(rank, suit)
+    @rank = rank.to_s.upcase
+    @suit = suit.downcase
   end
 end
+
+class Deck
+  def initialize
+    @box = []
+    Card::SUITS.each do |suit|
+      Card::RANKS.each do |rank|
+        box = Card.new(rank, suit)
+        @box.push(box)
+      end
+    end
+  end
+
+  def show
+    @box.each do |c|
+      puts "#{c.rank} #{c.suit}"
+    end
+  end
+
+  def cards
+    @box
+  end
+end
+
+deck = Deck.new
+deck.show
+
